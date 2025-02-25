aedd8f7a0daf163b09c797ab8e10756fce8a4d51
cyberpsychofc <aryan2003k@gmail.com>
2025-02-25 22:37:43 +0530

    centralization of code

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 import time
 import threading
 import schedule
-from .tweet import tweet
+from groq import Groq
 import streamlit as st
+import random
+import tweepy
+from dotenv import load_dotenv
+
+load_dotenv()
 
 st.set_page_config(
     page_title="CyberpsychAI",
@@ -10,6 +15,31 @@ st.set_page_config(
 )
 
 st.title('CyberpsychAI')
+
+ACCESS_KEY = st.secrets["general"]["ACCESS_KEY"]
+ACCESS_SECRET = st.secrets["general"]["ACCESS_SECRET"]
+CONSUMER_KEY = st.secrets["general"]["CONSUMER_KEY"]
+CONSUMER_SECRET = st.secrets["general"]["CONSUMER_SECRET"]
+BEARER_TOKEN = st.secrets["general"]["BEARER_TOKEN"]
+GROQ_API_KEY = st.secrets["general"]["GROQ_API_KEY"]
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(
+    ACCESS_KEY,
+    ACCESS_SECRET,
+)
+
+newapi = tweepy.Client(
+    bearer_token= BEARER_TOKEN,
+    access_token= ACCESS_KEY,
+    access_token_secret= ACCESS_SECRET,
+    consumer_key= CONSUMER_KEY,
+    consumer_secret= CONSUMER_SECRET,
+)
+
+api = tweepy.API(auth)
+
+rivals = ['MistralAI','ChatGPTapp','deepseek_ai','AnthropicAI','GeminiApp','github','MSFTCopilot','Apple']
 
 # Free-tier sends atmost of 17 requests a day, so plan
 
@@ -20,6 +50,73 @@ def run_scheduler():
 
 def tweet_job():
     schedule.every(3).minutes.do(tweet)
+
+llm = Groq(api_key=GROQ_API_KEY)  # LLM initialization
+
+psychs = ['Artifical Intelligence', 'Philosophy', 'Financial Advices']
+
+prompt = f"""
+You are an expert on the topic of {random.choice(psychs)}, tell me a funny or uncommon fact about
+it in less than 250 characters. Your response should be precise, avoid using any unnecessary words.
+"""
+
+roast = """
+You are a professional comedian. {} is your rival Artifical Intelligence company.
+Roast them in less than 250 words. The context is {}.
+"""
+
+model_name = "llama3-8b-8192"
+
+
+def generate_post_text():
+    tweet = llm.chat.completions.create(
+        messages=[
+            {
+                'role':'system',
+                'content': prompt
+            }
+        ],
+        model=model_name,
+    )
+    return tweet.choices[0].message.content
+
+def generate_reply_text(username, context):
+    reply = llm.chat.completions.create(
+        messages=[
+            {
+                'role':'system',
+                'content': roast.format(username, context)
+            }
+        ],
+        model=model_name,
+    )
+    return reply.choices[0].message.content
+
+def tweet():
+    try:
+        sampletweet = generate_post_text()
+        
+        post_result = newapi.create_tweet(text=sampletweet)
+    
+    except Exception as e:
+        print(f"Tweet couldn't be posted because: {e}")
+
+def reply():
+    try:
+        username = random.choice(rivals)
+        tweets = newapi.search_recent_tweets(
+            query=f"from:{username} -is:retweet -is:reply", 
+            max_results=10, 
+            tweet_fields=["id", "text"])
+        
+        if tweets:
+            latest_tweet = random.choice(tweets.data)  
+            tweet_id = latest_tweet.id  
+            tweet_text = latest_tweet.text  
+            newapi.create_tweet(in_reply_to_tweet_id=tweet_id, text=generate_reply_text(username,tweet_text))
+
+    except Exception as e:
+        print(f"Reply couldn't be posted because: {e}")
 
 if __name__ == "__main__":
     tweet_job()
