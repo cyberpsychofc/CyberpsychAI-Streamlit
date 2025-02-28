cf969aa5f1108ee4dbe78f3b97b2e160c4dc1d12
Om Aryan <aryan2003k@gmail.com>
2025-02-28 07:28:31 +0530

    changed thread lock final

--- app.py
+++ app.py
@@ -43,9 +43,9 @@ api = tweepy.API(auth)
 llm = Groq(api_key=GROQ_API_KEY)  # LLM initialization
 
 scheduler_thread = None
-scheduler_thread_lock = threading.Lock() 
+scheduler_thread_lock = threading.Lock() # avoids multiple threads to be created
 
-post_times = ["01:54","01:55","01:56","01:57","01:58","01:30","03:30","05:30","07:30","09:30",
+post_times = ["01:30","03:30","05:30","07:30","09:30",
               "11:30","13:30","15:30","17:30","19:30","21:30","23:30"]  # Instance timezone is UTC
 
 # rivals = ['MistralAI','ChatGPTapp','deepseek_ai','AnthropicAI','GeminiApp','github','MSFTCopilot','Apple']
@@ -103,13 +103,10 @@ def tweet():
         sampletweet = generate_post_text()
         post_result = newapi.create_tweet(text=sampletweet)
         logging.info(f"Tweet posted: {post_result.data['id']}")
-        time.sleep(600)
     except Exception as e:
         logging.error(f"Tweet couldn't be posted: {e}")
 
 tweet_job()
-
-state_lock = threading.Lock() # avoids multiple threads to be created
 
 with scheduler_thread_lock:
     if scheduler_thread is None:
