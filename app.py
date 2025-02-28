0afd3971341623f982613a9b990004572763b904
Om Aryan <aryan2003k@gmail.com>
2025-02-28 17:41:20 +0530

    threading scheme fix

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ scheduler_thread = None
 scheduler_thread_lock = threading.Lock() # avoids multiple threads to be created
 scheduler_running = False
 
-post_times = ["11:58","11:59","12:00","12:01","12:02","12:03","12:04","12:05","12:06","01:30","03:30","05:30","07:30","09:30",
+post_times = ["01:30","03:30","05:30","07:30","09:30",
               "11:30","13:30","15:30","17:30","19:30","21:30","23:30"]  # Instance timezone is UTC
 
 # rivals = ['MistralAI','ChatGPTapp','deepseek_ai','AnthropicAI','GeminiApp','github','MSFTCopilot','Apple']
