71d1cf8e4a0e07645aa78ec0e843fa9b4e8c067a
Om Aryan <aryan2003k@gmail.com>
2025-03-03 22:52:15 +0530

    add interval. increase retry wait to a minute

--- app.py
+++ app.py
@@ -52,8 +52,8 @@ scheduler_thread = None
 scheduler_thread_lock = threading.Lock() # avoids multiple threads to be created
 scheduler_running = False
 
-post_times = ["01:30","03:30","05:30","07:30","09:30",
-              "11:30","13:30","15:30","17:30","19:30","21:30","23:30"]  # Instance timezone is UTC
+post_times = ["01:30","03:30","04:30","05:30","07:30","09:30",
+              "11:30","12:30","13:30","15:30","17:30","19:30","21:30","23:30"]  # Instance timezone is UTC
 
 # rivals = ['MistralAI','ChatGPTapp','deepseek_ai','AnthropicAI','GeminiApp','github','MSFTCopilot','Apple']
 
@@ -145,7 +145,7 @@ def tweet():
     except Exception as e:
         logging.error(f"Tweet couldn't be posted: {e}")
         if "403" in str(e):
-            time.sleep(10) # try after 5 seconds
+            time.sleep(60) # try after a minute
             logging.info("Retrying...")
             try:
                 post_result = newapi.create_tweet(text=sampletweet)
